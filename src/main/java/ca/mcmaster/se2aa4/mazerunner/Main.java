f4e9365ca9b2712d04b0bd996d24b1ff75e0979e
Syed Abbas <syed.abbas14864@gmail.com>
2025-01-21 17:46:30 -0500

    parse maze file and store walls/spaces as 1s and 0s in a 2d array

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,36 +1,60 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger log = LogManager.getLogger();
+    private static final MazeReader reader = new MazeReader();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        log.info("** Starting Maze Application");
+
+        Options cliOptions = new Options();
+
+        Option fileOption = Option.builder("i")
+                .longOpt("input file")
+                .desc("Path to the maze input file")
+                .hasArg()
+                .argName("MAZE_FILE")
+                .required(true)
+                .build();
+        cliOptions.addOption(fileOption);
+
+        CommandLineParser cmdParser = new DefaultParser();
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
-                    }
+            CommandLine cmd = cmdParser.parse(cliOptions, args);
+
+            String mazePath = cmd.getOptionValue("i");
+
+            int[][] maze = reader.loadMaze(mazePath);
+
+            System.out.println("**** Maze Layout ****");
+            for (int[] row : maze) {
+                for (int cell : row) {
+                    System.out.print(cell + " ");
                 }
-                System.out.print(System.lineSeparator());
+                System.out.println();
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+
+        } catch (ParseException e) {
+            log.error("Failed to parse command-line arguments: " + e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("MazeRunner", cliOptions);
+        } catch (RuntimeException e) {
+            log.error("Unexpected error occurred", e);
+            e.printStackTrace();
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+
+        log.info("**** End of Maze Application");
     }
 }
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeReader.java
@@ -0,0 +1,52 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MazeReader {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public int[][] loadMaze(String mazeFile) {
+        System.out.println("** Initializing Maze Loader");
+
+        try {
+            log.info("**** Loading maze from file: " + mazeFile);
+            BufferedReader fileReader = new BufferedReader(new FileReader(mazeFile));
+
+            int maxRows = 0;
+            int maxCols = 0;
+            String line;
+
+            while ((line = fileReader.readLine()) != null) {
+                maxRows++;
+                maxCols = Math.max(maxCols, line.length());
+            }
+
+            fileReader.close();
+
+            int[][] mazeGrid = new int[maxRows][maxCols];
+
+            BufferedReader reader = new BufferedReader(new FileReader(mazeFile));
+            int rowIndex = 0;
+
+            while ((line = reader.readLine()) != null) {
+                for (int colIndex = 0; colIndex < line.length(); colIndex++) {
+                    mazeGrid[rowIndex][colIndex] = (line.charAt(colIndex) == ' ') ? 1 : 0;
+                }
+                rowIndex++;
+            }
+
+            reader.close();
+            return mazeGrid;
+
+        } catch (IOException e) {
+            log.error("Error loading maze file", e);
+            return null;
+        }
+    }
+}
